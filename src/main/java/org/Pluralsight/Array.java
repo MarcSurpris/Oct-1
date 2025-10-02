bd84ac0115827f4e7a74baf149a549e090354b8d
w3schools-test <test@w3schools.com>
2025-10-02 13:14:33 -0400

    w

---
+++ src/main/java/org/Pluralsight/Array.java
@@ -0,0 +1,9 @@
+package org.Pluralsight;
+
+public class Array {
+    public static void main(String[] args){
+        int firstNumber = 1;
+        int[] listOfNumbers = new int[5];
+        int[] listOfNumbers = {63, 123, 234, 123, 545, 23, 64};
+    }
+}
